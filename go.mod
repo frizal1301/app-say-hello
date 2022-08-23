ef322ba136f3c0c878841dc899f120f42b290a02
frizal1301 <frizalichal01@gmail.com>
2022-08-23 09:36:46 +0800

    menambah module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/frizal1301/app-say-hello
+
+go 1.18
+
+require github.com/frizal1301/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/frizal1301/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
